d98d5204887c9895b0c619e36c32f4970c8489b6
JoseAntonioOrtega04 <122041661@upq.edu.mx>
2025-03-05 06:39:14 +0000

    Agregada app Flask para despliegue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "El despliegue con Jenkins y Docker fue correcto 🚀"
+    return "El despliegue con Jenkins y Docker fue correcto"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
